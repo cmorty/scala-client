d9872bcb0b55ec4f6e8ff932ecf0efd3e49ba5df
Moritz 'Morty' Strübe <morty@gmx.net>
2013-03-20 11:00:35 +0100

    Fix bug where messages were sometimes submitted multiple times

--- src/main/scala/de/fau/wisebed/messages/MsgLiner.scala
+++ src/main/scala/de/fau/wisebed/messages/MsgLiner.scala
@@ -9,16 +9,24 @@ trait MsgLiner extends MessageInput {
 	private var mbuf = Map[String, Message]()
 
 	abstract override def handleMsg(msg: Message) {
+		//Check whether we have some old data
 		val wmsg = mbuf.get(msg.node) match {
 			case Some(old) => {
+				//Remove from map
+				mbuf -= msg.node
+				//Add new data to old data
 				old.data ++= msg.data
+				//Update timestamp
 				old.timestamp = msg.timestamp
 				old
 			}
+			//If there is no old mes just us it
 			case _ => msg
 		}
 		
+		// Get data
 		val str = wmsg.dataString
+		// Split at CR
 		val split = str.split("\n")
 		
 		split.foreach(x => {
