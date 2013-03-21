816d81b92a3fa0ef2d21800cc544f5ee5a72bcdd
Moritz 'Morty' Strübe <morty@gmx.net>
2013-03-21 16:21:59 +0100

    Yet another version if MsgLiner

--- src/main/scala/de/fau/wisebed/messages/MsgLiner.scala
+++ src/main/scala/de/fau/wisebed/messages/MsgLiner.scala
@@ -4,39 +4,27 @@ import eu.wisebed.api._
 import de.fau.wisebed.wrappers.WrappedMessage._
 import scala.collection
 import eu.wisebed.api.common.Message
+import scala.collection.mutable.ArrayBuffer
 
 trait MsgLiner extends MessageInput {
-	private var mbuf = Map[String, Message]()
+	private var mbuf = Map[String, ArrayBuffer[Byte]]()
 
 	abstract override def handleMsg(msg: Message) {
-		//Check whether we have some old data
-		val wmsg = mbuf.get(msg.node) match {
-			case Some(old) => {
-				//Remove from map
-				mbuf -= msg.node
-				//Add new data to old data
-				old.data ++= msg.data
-				//Update timestamp
-				old.timestamp = msg.timestamp
-				old
-			}
-			//If there is no old mes just us it
-			case _ => msg
-		}
 		
-		// Get data
-		val str = wmsg.dataString
-		// Split at CR
-		val split = str.split("\n")
+		var outp =  mbuf.getOrElse(msg.node, ArrayBuffer[Byte]())
 		
-		split.foreach(x => {
-			val snd = wmsg.copy
-			snd.dataString = x
-			if(x != split.last || str.last == '\n'){
+		for(e <- msg.data){
+			if(e == '\n'){
+				val snd = msg.copy
+				snd.data = outp.toArray
+				outp = ArrayBuffer[Byte]()
 				super.handleMsg(snd)
 			} else {
-				mbuf += wmsg.node -> snd
+				outp += e
 			}
-		})
+		}
+
+		mbuf += msg.node -> outp
+
 	}
 }
